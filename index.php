52b7ef35f8ed6b8972d9f56327226a1c7c4a0671
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-03 10:24:17 -0400

    started basic file

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+	
+
+	<head>
+		<title>Address Book</title>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+	</head>
+
+	<body>
+
+		<header>
+			Address Book
+		</header>
+
+		<script>
+
+		var Sam = {
+			firstName: Sam;
+			lastName: Sanchez;
+			address: "7777 Nice Street";
+		}
+
+
+		</script>
+	</body>		
+</html>	
