af237b44a6e496642b74791fdfd2bd57e260061e
Mark Goldberg <mgoldberg@launchdm.com>
2016-06-03 10:59:13 -0400

    added button and function (function/button combo not working)

--- index.php
+++ index.php
@@ -15,13 +15,24 @@
 
 		<script>
 
-		var Sam = {
-			firstName: Sam;
-			lastName: Sanchez;
-			address: "7777 Nice Street";
-		}
+			var Sam = {
+				firstName: Sam;
+				lastName: Sanchez;
+				address: "7777 Nice Street";
+			}
 
+			function search() {
+				return "text";
+			}
 
 		</script>
+
+		<div>
+			<b>Last Name:</b> <input type="text" name="firstName" value=""/><br />
+			<button onclick="getElementById('result').innerHTML=search()">Search</button>
+		</div>
+
+		<p id = result></p>
+
 	</body>		
 </html>	
